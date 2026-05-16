e53aef0eed395de4cede81b9dd041d1cad8a989e
flz632ylrm@gmail.com <flz632ylrm@gmail.com>
2026-05-16 16:57:08 +0300

    Veri tabanindaki student tablosunu class yapisi ile tanimlama

---
+++ Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace Club_Otomasyon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_ekle = new System.Windows.Forms.Button();
+            this.btn_sil = new System.Windows.Forms.Button();
+            this.btn_guncelle = new System.Windows.Forms.Button();
+            this.btn_listele = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txt_email = new System.Windows.Forms.TextBox();
+            this.txt_telefon = new System.Windows.Forms.TextBox();
+            this.soyisim = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_isim = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Name = new System.Windows.Forms.Label();
+            this.cmb_bolum = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(992, 209);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btn_ekle
+            // 
+            this.btn_ekle.Location = new System.Drawing.Point(25, 603);
+            this.btn_ekle.Name = "btn_ekle";
+            this.btn_ekle.Size = new System.Drawing.Size(114, 40);
+            this.btn_ekle.TabIndex = 1;
+            this.btn_ekle.Text = "Ekle";
+            this.btn_ekle.UseVisualStyleBackColor = true;
+            // 
+            // btn_sil
+            // 
+            this.btn_sil.Location = new System.Drawing.Point(185, 603);
+            this.btn_sil.Name = "btn_sil";
+            this.btn_sil.Size = new System.Drawing.Size(105, 40);
+            this.btn_sil.TabIndex = 2;
+            this.btn_sil.Text = "Sil";
+            this.btn_sil.UseVisualStyleBackColor = true;
+            // 
+            // btn_guncelle
+            // 
+            this.btn_guncelle.Location = new System.Drawing.Point(336, 603);
+            this.btn_guncelle.Name = "btn_guncelle";
+            this.btn_guncelle.Size = new System.Drawing.Size(104, 40);
+            this.btn_guncelle.TabIndex = 3;
+            this.btn_guncelle.Text = "Güncelle";
+            this.btn_guncelle.UseVisualStyleBackColor = true;
+            // 
+            // btn_listele
+            // 
+            this.btn_listele.Location = new System.Drawing.Point(479, 603);
+            this.btn_listele.Name = "btn_listele";
+            this.btn_listele.Size = new System.Drawing.Size(92, 40);
+            this.btn_listele.TabIndex = 4;
+            this.btn_listele.Text = "Listele";
+            this.btn_listele.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cmb_bolum);
+            this.groupBox1.Controls.Add(this.txt_email);
+            this.groupBox1.Controls.Add(this.txt_telefon);
+            this.groupBox1.Controls.Add(this.soyisim);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txt_isim);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.Name);
+            this.groupBox1.Location = new System.Drawing.Point(25, 227);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(979, 349);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // txt_email
+            // 
+            this.txt_email.Location = new System.Drawing.Point(237, 252);
+            this.txt_email.Name = "txt_email";
+            this.txt_email.Size = new System.Drawing.Size(202, 22);
+            this.txt_email.TabIndex = 9;
+            // 
+            // txt_telefon
+            // 
+            this.txt_telefon.Location = new System.Drawing.Point(237, 134);
+            this.txt_telefon.Name = "txt_telefon";
+            this.txt_telefon.Size = new System.Drawing.Size(202, 22);
+            this.txt_telefon.TabIndex = 7;
+            // 
+            // soyisim
+            // 
+            this.soyisim.Location = new System.Drawing.Point(237, 71);
+            this.soyisim.Name = "soyisim";
+            this.soyisim.Size = new System.Drawing.Size(202, 22);
+            this.soyisim.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 259);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "E-Mail";
+            // 
+            // txt_isim
+            // 
+            this.txt_isim.Location = new System.Drawing.Point(237, 22);
+            this.txt_isim.Name = "txt_isim";
+            this.txt_isim.Size = new System.Drawing.Size(202, 22);
+            this.txt_isim.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 201);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Bölüm";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Telefon Numarası";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Soyad";
+            // 
+            // Name
+            // 
+            this.Name.AutoSize = true;
+            this.Name.Location = new System.Drawing.Point(20, 28);
+            this.Name.Name = "Name";
+            this.Name.Size = new System.Drawing.Size(24, 16);
+            this.Name.TabIndex = 0;
+            this.Name.Text = "Ad";
+            // 
+            // cmb_bolum
+            // 
+            this.cmb_bolum.FormattingEnabled = true;
+            this.cmb_bolum.Location = new System.Drawing.Point(237, 192);
+            this.cmb_bolum.Name = "cmb_bolum";
+            this.cmb_bolum.Size = new System.Drawing.Size(202, 24);
+            this.cmb_bolum.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1027, 677);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btn_listele);
+            this.Controls.Add(this.btn_guncelle);
+            this.Controls.Add(this.btn_sil);
+            this.Controls.Add(this.btn_ekle);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_ekle;
+        private System.Windows.Forms.Button btn_sil;
+        private System.Windows.Forms.Button btn_guncelle;
+        private System.Windows.Forms.Button btn_listele;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Name;
+        private System.Windows.Forms.TextBox txt_isim;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_email;
+        private System.Windows.Forms.TextBox txt_telefon;
+        private System.Windows.Forms.TextBox soyisim;
+        private System.Windows.Forms.ComboBox cmb_bolum;
+    }
+}
+
---
+++ student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Club_Otomasyon
+{
+    [Table("Table_students")]
+    internal class student
+    {
+        [Key]
+        public int student_ıd { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string student_name { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string student_surname { get; set; }
+
+        [Required]
+        [MaxLength(11)]
+        public string student_phone { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string student_department { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string student_email { get; set; }    
+    }
+}
